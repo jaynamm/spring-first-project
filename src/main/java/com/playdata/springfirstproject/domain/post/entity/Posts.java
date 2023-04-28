87c2eb3bda7aca6013ad43a91c9faf44ebb7f47d
jaynamm <polky0421@gmail.com>
2023-04-28 14:31:49 +0900

    Feat: Posts 엔티티 생성

---
+++ src/main/java/com/playdata/springfirstproject/domain/post/entity/Posts.java
@@ -0,0 +1,31 @@
+package com.playdata.springfirstproject.domain.post.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+    @Id // 테이블의 PK 를 의미한다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    private Long id; // Long => bigint type
+
+    @Column(length = 500, nullable = false) // 기본으로 255 자로 되어있다.
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
